0ee32e733532ed92541ef22bd8949e3f7e63afa5
gomamon <leeye9779@gmail.com>
2018-10-24 23:51:24 +0900

    revival set_token()

--- main.js
+++ main.js
@@ -2,73 +2,79 @@ var token;
 var devID;
 var login_flag=false;
 
-//인아의 말 = 테스트해보려고 제껄로 토큰을 다 넣었습니다!
+
+window.onloded
 
 function set_token(){
-	PushBullet.APIKey= "o.IWTQpSmMDgY4nakyuAc1ErJ7euO4QUih";
-        let res = PushBullet.devices();
-        devID = res.devices[0].iden;	
+	PushBullet.APIKey= token.value;
+    let res = PushBullet.devices();
+    devID = res.devices[0].iden;	
 }
 window.onload=set_token;
 
 function login() {
-	token = document.getElementById("token");
+    token = document.getElementById("token");
     PushBullet.APIKey= token.value;
-	
-	login_flag =true;	// 사실 필요 없음
-	
+
+    login_flag =true;
+
     let res = PushBullet.devices();
     devID = res.devices[0].iden;
-	alert("login to PushBullet on WebOS");
-	location.href="../frame.html";
+    alert("login to PushBullet on WebOS");
+    location.href="../frame.html";
+
+
 }
 
+
 function sendMe() {
-        let msg = document.getElementById("msg");
-        PushBullet.push("note", devID, null, {title: "MSG", body: msg.value});
-    }
+    let msg = document.getElementById("msg");
+    PushBullet.push("note", devID, null, {title: "MSG", body: msg.value});
+}
 
 function sendMeFile() {
     document.getElementById('files').addEventListener('change', function(e) {
-    var file = this.files[0];
-    PushBullet.pushFile(devID, null, file, "pushing files", function(err, res) {
-    if(err) {
-        throw err;
-    } else {
-    }
-    });
-});
-}
-
-    function sendFriends() {
-        let msg_to = document.getElementById("msg");
-        let email = document.getElementById("email");
-        PushBullet.push("link", null, email.value, {title: "MSG", body: msg_to.value});
-    }
-
-    function get_user() {
-        var res = PushBullet.user();
-        document.getElementById("user").innerHTML = "user : "+ res.email ;
-     }
-
-     function get_push(i) {
-         PushBullet.pushHistory(function(err, res) {
-            if(err){
+        var file = this.files[0];
+        PushBullet.pushFile(devID, null, file, "pushing files", function(err, res) {
+            if(err) {
                 throw err;
-            }
-            else {
-                if( i< res.pushes.length)
-                    return res.pushes[i].body;
+            } else {
             }
         });
-     }
+    });
+}
 
- function del_history() {
+
+
+function sendFriends() {
+    let msg_to = document.getElementById("msg");
+    let email = document.getElementById("email");
+    PushBullet.push("link", null, email.value, {title: "MSG", body: msg_to.value});
+}
+
+function get_user() {
+    var res = PushBullet.user();
+    document.getElementById("user").innerHTML = "user : "+ res.email ;
+}
+
+function get_push(i) {
+    PushBullet.pushHistory(function(err, res) {
+        if(err){
+            throw err;
+        }
+        else {
+            if( i< res.pushes.length)
+                return res.pushes[i].body;
+        }
+    });
+}
+
+function del_history() {
     document.getElementById("history").innerHTML = "delete ";
     PushBullet.pushHistory(function(err, res) {
-    if(err) {
-        throw err;
-    } else {
+        if(err) {
+            throw err;
+        } else {
             let res =PushBullet.pushHistory();
             for(var i=0; i<res.pushes.length; i++) {
                 if(res.pushes[i].active) {
@@ -82,8 +88,8 @@ function sendMeFile() {
         }
     });
 }
-
- function get_history() {
+/*
+function get_history() {
     PushBullet.pushHistory(function(err, res) {
         if(err){
             throw err;
@@ -100,15 +106,15 @@ function sendMeFile() {
             document.getElementById("history").innerHTML = push;
         }
     });
- }
-
+}
+*/
 
 //
-  function gonext(){
-        location.href = "test.html";
+function gonext(){
+    location.href = "test.html";
 }
 
 function goback(){
-        //window.history.back();
-        location.href="frame.html";
+    //window.history.back();
+    location.href="../frame.html";
 }
